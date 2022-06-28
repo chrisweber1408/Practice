1929930c709a7ff00c8eae83801fac4e509baeb6
chris <chrisweber1991@gmail.com>
2022-06-28 17:45:00 +0200

    security Token

--- backend/src/main/java/com/example/demo/security/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.example.demo.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -15,17 +19,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     private final MongoUserDetailsService userDetailsService;
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .and().formLogin();
+                .antMatchers(HttpMethod.POST, "/api/user", "/api/login").permitAll()
+                .antMatchers("/**").authenticated()
+                .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
     }
 
     @Override
--- backend/src/main/java/com/example/demo/user/MyUserController.java
+++ backend/src/main/java/com/example/demo/user/MyUserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.security.Principal;
+
 
 @RestController
 @RequestMapping("api/user")
@@ -11,14 +14,22 @@ import java.util.Optional;
 public class MyUserController {
 
     private final MyUserService myUserService;
+    private final PasswordEncoder encoder;
 
         @PostMapping
         public void postNewUser(@RequestBody MyUser newUser){
+            String password = encoder.encode(newUser.getPassword());
+            newUser.setPassword(password);
             myUserService.addNewUser(newUser);
         }
         @GetMapping("/{username}")
         public MyUser getUser(@PathVariable String username){
             return myUserService.getByUsername(username);
+        }
+
+        @GetMapping("/me")
+        public MyUser findMe(Principal principal){
+            return myUserService.getByUsername(principal.getName());
         }
 
 }
